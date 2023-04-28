27ca1cfda5690836181128294e3b8b4055b8d615
Thaisa Alice <thaisalic3@icloud.com>
2023-04-27 22:25:46 -0300

    feat: removed cards static and added api

--- src/app/pages/view/view.component.ts
+++ src/app/pages/view/view.component.ts
@@ -11,95 +11,27 @@ export class ViewComponent implements OnInit{
 
   cards=[
     {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: 'Trakto Design - Perfeito para empreendedores',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: 'Trakto para Empresas',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: '4 formas de ganhar dinheiro com a Trakto!',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: 'Confira as NOVIDADES e ATUALIZAÇÕES da TRAKTO',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: 'Como criar e publicar o Trakto Link',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: 'Como criar story animado',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: 'Trakto Design - Perfeito para empreendedores',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: 'Trakto para Empresas',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: '4 formas de ganhar dinheiro com a Trakto!',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: 'Confira as NOVIDADES e ATUALIZAÇÕES da TRAKTO',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: 'Como criar e publicar o Trakto Link',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: 'Como criar story animado',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: 'Trakto Design - Perfeito para empreendedores',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: 'Trakto para Empresas',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: '4 formas de ganhar dinheiro com a Trakto!',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: 'Confira as NOVIDADES e ATUALIZAÇÕES da TRAKTO',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: 'Como criar e publicar o Trakto Link',
-    },
-    {
-      imageSrc: 'https://via.placeholder.com/200',
-      subtitle: 'Como criar story animado',
+      imageSrc: '',
+      subtitle: '',
     },
   ]
 
   constructor(private service: ServiceTrakto) {}
 
   ngOnInit(): void {
-    // this.service.SlideAll({}).subscribe(
-    //   (response: any) => {
-    //     this.cards=response.data.map((card: any) => ({
-    //       id: card.id,
-    //       imageSrc: card.thumbs.raw,
-    //       subtitle: card.title,
-    //     }));
-    //     this.isLoading=false;
-    //   },
-    //   (error: any) => {
-    //     console.log('Error:', error);
-    //     this.isLoading=false;
-    //   }
-    // );
+    this.service.SlideAll({}).subscribe(
+      (response: any) => {
+        this.cards=response.data.map((card: any) => ({
+          id: card.id,
+          imageSrc: card.thumbs.raw,
+          subtitle: card.title,
+        }));
+        this.isLoading=false;
+      },
+      (error: any) => {
+        console.log('Error:', error);
+        this.isLoading=false;
+      }
+    );
   }
 }
